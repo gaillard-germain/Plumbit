44a2875b7f129f4e8cf8f3e7c363ae60894e159f
Gaillard Germain <gaillard.germain@gmail.com>
2022-07-30 19:32:36 +0200

    added more infos.

--- app/game.py
+++ app/game.py
@@ -13,10 +13,14 @@ from sprites.stamp import Stamp
 class Game:
     INFO = [
         'Countdown starts when you place the first pipe.',
-        'The golden cross doubles your gain but speeds up the liquid.',
-        'The chance to have a golden cross increases with the level.',
+        'The golden-cross doubles your gains but speeds up the liquid.',
+        'The chance to have a golden-cross increases with the level.',
         'When the liquid crosses a cross it goes straight.',
-        'Time decreases by 5 seconds every 5 levels.'
+        'Time decreases by 5 seconds every 5 levels.',
+        'The Flood button starts the liquid immediately.',
+        'The give-up button brings you back to the menu.',
+        'You cannot replace a flooded pipe.',
+        'The number of obstacles increases with the level.'
     ]
 
     def __init__(self, screen):
