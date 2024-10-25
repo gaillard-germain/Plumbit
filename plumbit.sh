dbca88ce9682e7beac4d4dac8a27c0dbd36c8ee4
gaillard-germain <germain@gaillard.dev>
2024-10-25 22:53:52 +0200

    Modify bash script

--- plumbit.sh
+++ plumbit.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 current_path=$( dirname ${BASH_SOURCE[0]}) && pwd
-cd $current_path
-source .venv/bin/activate
+cd "$current_path" || exit
+
+if [ ! -d ".venv/" ]; then
+  python3 -m venv .venv
+  source .venv/bin/activate
+  pip install -r requirements.txt
+else
+  source .venv/bin/activate
+fi
+
 python3 run.py
